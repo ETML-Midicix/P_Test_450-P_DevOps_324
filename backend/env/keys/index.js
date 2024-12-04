d027519d0851cb4fe72395fc00f2d1fae44def5d
JulienETML-hub <119680240+JulienETML-hub@users.noreply.github.com>
2024-12-04 15:22:49 +0100

    Update index.js

--- backend/env/keys/index.js
+++ backend/env/keys/index.js
@@ -1,6 +1,13 @@
 const fs = require('fs');
 
 module.exports = {
+    key: fs.readFileSync('./jwtRS256.key'),
+    keyPub: fs.readFileSync('./jwtRS256.key.pub'),
+}
+/*
+const fs = require('fs');
+
+module.exports = {
     key: fs.readFileSync('./backend/env/keys/jwtRS256.key'),
     keyPub: fs.readFileSync('./backend/env/keys/jwtRS256.key.pub'),
-}
+}*/
