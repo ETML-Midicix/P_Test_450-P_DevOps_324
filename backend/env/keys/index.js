930d759a6fc9572b83cad364cfdd523343c0d401
ETML-Midicix <119678382+ETML-Midicix@users.noreply.github.com>
2024-12-11 16:21:06 +0100

    fix(index.js) change keys path

--- backend/env/keys/index.js
+++ backend/env/keys/index.js
@@ -1,16 +1,16 @@
 // Chemin pour que ça marche sur docker mais pas localement
-const fs = require('fs');
+/*const fs = require('fs');
 
 
 module.exports = {
     key: fs.readFileSync('./backend/env/keys/jwtRS256.key'),
     keyPub: fs.readFileSync('./backend/env/keys/jwtRS256.key.pub'),
-}
+}*/
 
 // Chemin pour que ça marche en local mais pas docker
-/*const fs = require('fs');
+const fs = require('fs');
 
 module.exports = {
     key: fs.readFileSync('./env/keys/jwtRS256.key'),
     keyPub: fs.readFileSync('./env/keys/jwtRS256.key.pub'),
-}*/
+}
